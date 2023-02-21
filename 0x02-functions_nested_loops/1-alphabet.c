802a856e7b2493cc23876961c40df7ed019ecd90
Your Name <you@example.com>
2023-02-21 15:09:47 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: 0-
+ */
+void print_alphabet(void);
+int main(void)
+{
+	char alph = 'a';
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		putchar (void print_alphabet(void);
+				putchar ("\n")
+	}
+	return(0);
+}
