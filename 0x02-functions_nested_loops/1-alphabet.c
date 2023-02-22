311e8f97e53e5d5c23585ae555fa7b43ea8b632c
Your Name <you@example.com>
2023-02-22 14:13:00 -0800

     1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,21 +4,17 @@
 /**
  * main - Prints the alphabet, in lowercase, followed by a new line
  *
- * Return: 0-
+ * Return: 0
  */
 void print_alphabet(void);
 int main(void)
 {
 	char alph = 'a';
-	int = 0;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
-		if (i <= 26 && i > 0)
-		{
-			putchar(alph);
-		}
-		putchar("\n");
+		putchar(alpha);
+	putchar('\n');
 	}
-	return(0);
+	return (0);
 }
