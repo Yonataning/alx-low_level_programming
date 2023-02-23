ce105cada7b551b5ce9fee3c50f6d471cd82a1fb
Your Name <you@example.com>
2023-02-23 13:40:56 -0800

    Task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+void main(int j)
+{
+	if (j >)
+		printf("%d is positive\n")
+	if (j <)
+		printf("%d is negative\n")
+	else
+		printf("%d is zero\n")
+
+	return (0);
+}
