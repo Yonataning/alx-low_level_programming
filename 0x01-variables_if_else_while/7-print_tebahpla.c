a6ab85f1a67bd15b37989a225327bc9c8e0e2ecb
Your Name <you@example.com>
2023-02-25 20:46:41 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints lower alphabet letters in reverse ending with new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int k = 'z';
+
+	while (k >= 'a')
+	{
+		putchar(k);
+		k--;
+	}
+	putchar('\n');
+		return (0);
+}
+	
