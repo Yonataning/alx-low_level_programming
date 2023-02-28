bbb97150281880b5dace6d89521090be6275deaa
Your Name <you@example.com>
2023-02-28 12:32:19 -0800

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 
+void reset_to_98(int *ptr)
+{
+	*ptr = 98;
+}
 /**
  * main - Takes pointer to int as parameter & updates  value it points to 98
  *
  * Return: 0
  */
-void reset_to_98(int *i);
 int main(void)
 {
 	int i;
+	int *ptr = &i;
 
 	i = 402;
-	printf("i=%d\n", i);
-	reset_to_98(&i);
-	printf("i=%d\n", i);
+	printf("ptr=%d\n", *ptr);
+	reset_to_98(ptr);
+	printf("ptr=%d\n", *ptr);
 	return (0);
 }
