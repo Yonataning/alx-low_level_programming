bf23ab334d63ce0479f083c5139108bd9369db37
Your Name <you@example.com>
2023-03-04 13:47:22 -0800

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,18 +6,17 @@
  * Return: 0
  */
 void print_alphabet(void);
-int main(void)
+
 {
 
 	char c = 'a';
 
 	while (c <= 'z')
 	{
-		putchar("%c", c);
+		putchar(c);
 	c++;
 	}
-	putchar("\n");
-	print_alphabet();
+	putchar('\n');
+	
 	return (0);
 }
-
