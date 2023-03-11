0e9fb922c1e5fe365ac47d1edc4095b9c76b85a4
Your Name <you@example.com>
2023-03-10 18:00:00 -0800

    Task 0

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,10 +7,12 @@
  * Return: factorial of n
  */
 int factorial(int n)
-i{
+{
+	int n; 
+
 	if (n < 0)
 		return (-1);
-	else if (n == 1)
+	 if (n == 1)
 		return (1);
 	return (n * (factorial(n-1));
 }
